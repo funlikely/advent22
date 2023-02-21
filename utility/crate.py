7a294c5f063c76b913181d9cb9fe01e334a42adb
Oldflat <ducksalt+github@gmail.com>
2023-02-20 21:19:00 -0500

    Problem 5 part 1 completed

---
+++ utility/crate.py
@@ -0,0 +1,22 @@
+def read_tops_of_crates(crates):
+    """Read the last part of each of a set of crates,
+       and returns a string of them in sequence. E.g., [['a', 'b'], ['c'], ['d', 'e', 'f']] becomes 'bcf'."""
+    tops_of_crates = [crate[-1:][0] for crate in crates]
+
+    return ''.join(tops_of_crates)
+
+
+def move_crate(crates, from_stack, to_stack):
+    """Note: stacks start with index 0."""
+    crate = crates[from_stack].pop()
+    crates[to_stack].append(crate)
+
+    return crates
+
+
+def move_crates(crates, from_stack, to_stack, crate_quantity):
+    """Note: stacks start with index 0."""
+    for i in range(crate_quantity):
+        crates = move_crate(crates, from_stack, to_stack)
+
+    return crates
